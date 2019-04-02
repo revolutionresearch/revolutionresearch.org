578498caaf978e453cabcdcf854dad0b77017d2a
Daniel Högel <hoegel.daniel@gmail.com>
2019-04-02 23:27:57 +0200

    Some feed styling and client logic

--- js/feed.js
+++ js/feed.js
@@ -38,68 +38,52 @@ class Post {
     }
 
     normalizePostData() {
-        switch (this.post.post_type) {
-            case 'post': {
-                const categories = this.post.categories.map(cat => cat.slug)
-                if (categories.includes('featured')) {
-                    console.log('featured post', this.post);
-                } else if (categories.includes('frage')) {
-                    console.log('fragen post', this.post);
+        const flockler = this.post.flockler;
+        switch (flockler.type) {
+            case 'tweet': {
+                const { tweet } = flockler.attachments;
+    
+                if (tweet.text.startsWith('Twitter http')) {
+                    const sharedLink = tweet.text.split('Twitter ')[1];
+                    this.text = `Twitter: <a href="${sharedLink}" target="_blank">${sharedLink}</a>`;
+                } else {
+                    this.text = tweet.text;
                 }
+                
+                this.timeCreated = new Date(tweet.created_at);
+                this.timeCreatedFormatted = formatDateTime(tweet.created_at);
+                this.mediaUrl = tweet.media_url;
+                this.mediaType = 'image';
+                this.mediaSourceUrl = this.postUrl;
+                this.author = `${tweet.name} (@${tweet.screen_name})`;
+                this.postUrl = `https://twitter.com/${this.post.screen_name}/status/${tweet.tweet_id_str}`;
+                this.profileImageUrl = tweet.profile_image_url;
                 break;
             }
-            case 'flockler_post': {
-                const flockler = this.post.flockler;
-                switch (flockler.type) {
-                    case 'tweet': {
-                        const { tweet } = flockler.attachments;
-            
-                        if (tweet.text.startsWith('Twitter http')) {
-                            const sharedLink = tweet.text.split('Twitter ')[1];
-                            this.text = `Twitter: <a href="${sharedLink}" target="_blank">${sharedLink}</a>`;
-                        } else {
-                            this.text = tweet.text;
-                        }
-                        
-                        this.timeCreated = new Date(tweet.created_at);
-                        this.timeCreatedFormatted = formatDateTime(tweet.created_at);
-                        this.mediaUrl = tweet.media_url;
-                        this.mediaType = 'image';
-                        this.mediaSourceUrl = this.postUrl;
-                        this.author = `${tweet.name} (@${tweet.screen_name})`;
-                        this.postUrl = `https://twitter.com/${this.post.screen_name}/status/${tweet.tweet_id_str}`;
-                        this.profileImageUrl = tweet.profile_image_url;
-                        break;
-                    }
-        
-                    case 'facebook_post': {
-                        const { facebook_post } = flockler.attachments;
-        
-                        this.timeCreated = new Date(facebook_post.created_time);
-                        this.timeCreatedFormatted = formatDateTime(facebook_post.created_time);
-                        this.text = facebook_post.message;
-                        this.author = facebook_post.from_name;
-                        this.mediaUrl = facebook_post.picture;
-                        this.mediaSourceUrl = facebook_post.link;
-                        this.mediaType = facebook_post.post_type;
-                        this.postUrl = `https://www.facebook.com/${facebook_post.post_id_str}`;
-                        this.profileImageUrl = `https://graph.facebook.com/${facebook_post.from_id_str}/picture?type=square`;
-                        break;
-                    }
-        
-                    case 'article': {
-                        const article = flockler;
-                        this.timeCreated = new Date(article.published_at);
-                        this.timeCreatedFormatted = formatDateTime(article.published_at);
-                        this.text = article.body;
-                        this.author = article.author;
-                        this.postUrl = article.url;
-                        break;
-                    }
-        
-                    default:
-                        break;
-                }
+
+            case 'facebook_post': {
+                const { facebook_post } = flockler.attachments;
+
+                this.timeCreated = new Date(facebook_post.created_time);
+                this.timeCreatedFormatted = formatDateTime(facebook_post.created_time);
+                this.text = facebook_post.message;
+                this.author = facebook_post.from_name;
+                this.mediaUrl = facebook_post.picture;
+                this.mediaSourceUrl = facebook_post.link;
+                this.mediaType = facebook_post.post_type;
+                this.postUrl = `https://www.facebook.com/${facebook_post.post_id_str}`;
+                this.profileImageUrl = `https://graph.facebook.com/${facebook_post.from_id_str}/picture?type=square`;
+                break;
+            }
+
+            case 'article': {
+                const article = flockler;
+                this.timeCreated = new Date(article.published_at);
+                this.timeCreatedFormatted = formatDateTime(article.published_at);
+                this.text = article.body;
+                this.author = article.author;
+                this.postUrl = article.url;
+                break;
             }
 
             default:
@@ -110,7 +94,7 @@ class Post {
     createElement(index = 0) {
         this.element = document.createElement('div');
 
-        this.element.className = 'feedItem';
+        this.element.className = `feedItem feedItem--flockler feedItem--${this.type}`;
         this.element.style.animationDelay = `${index * 50}ms`
         this.element.dataset.rating = this.ratingValue;
         this.element.innerHTML = this.generateHtml();
@@ -121,8 +105,8 @@ class Post {
     generateHtml() {
         return `
             <div class="feedItem__debugInfos">
-                Rating: ${this.ratingValue},
-                Count: ${this.ratingCount},
+                Rating: <span class="rating-value">${this.ratingValue}</span>,
+                Count: <span class="rating-count">${this.ratingCount}</span>,
                 Created: ${this.timeCreatedFormatted}
             </div>
             <h3 class="feedItem__title">
@@ -153,9 +137,6 @@ class Post {
                     <input class="rating__input" value="0" id="${this.postId}" data-id="${this.postId}" type="range" min="-4" max="4" step="1" />
                     <span class="rating__emoji">😊</span>
                 </div>
-                <div class="rating__text">
-                    ${this.generateRatingText()}
-                </div>
             </div>
         
             <div class="feedItem__sharing">
@@ -167,18 +148,16 @@ class Post {
                 </a>
             </div>
         `;
-    }
+    } 
 
-    generateRatingText() {
-        return `${this.ratingCount} Bewertung${this.ratingsCount !== 1 ? 'en' : ''}: ${this.ratingValue || 0}`;
-    }   
-
-    updateRating({ value, count }) {
-        this.ratingValue = value;
-        this.ratingCount = count;
+    updateRating(ratingString) {
+        const ratings = ratingString.split(';');
+        this.ratingValue = ratings.reduce((acc, r) => acc + parseInt(r, 10), 0);
+        this.ratingCount = ratings.length;
 
         this.element.dataset.rating = this.ratingValue;
-        this.element.querySelector('.rating__text').innerHTML = this.generateRatingText();
+        this.element.querySelector('.feedItem__debugInfos .rating-value').innerText = this.ratingValue;
+        this.element.querySelector('.feedItem__debugInfos .rating-count').innerText = this.ratingCount;
     }
 }
 
@@ -187,9 +166,17 @@ class Post {
  * FEED CLASS
  */
 class Feed {
-    constructor(rootSelector) {
+    constructor(rootSelector, options) {
         this.rootSelector = rootSelector;
         this.root = document.querySelector(this.rootSelector);
+        
+        if (options.reloadButton) {
+            this.reloadButton = document.querySelector(options.reloadButton);
+        }
+
+        if (options.paginationsSelector) {
+            this.paginationsSelector = options.paginationsSelector;
+        }
 
         // dynamic propteries
         this.page = 0;
@@ -197,15 +184,12 @@ class Feed {
         this.isFetching = false;
         this.items = [];
         this.itemsElements = [];
-        this.questions = [];
-        this.questionIndex = 0;
         this.youIndex = 0;
 
         // binding
-        this.updateItems = this.updateItems.bind(this);
+        this.reloadPage = this.reloadPage.bind(this);
         this.updateRating = this.updateRating.bind(this);
-        this.testReordering = this.testReordering.bind(this);
-
+        
         // INIT
         if (this.root) {
             this.init();
@@ -214,9 +198,26 @@ class Feed {
 
    async  init() {
        // event listeners
-        document.addEventListener('scroll', this.updateItems);
-        this.root.addEventListener('change', this.updateRating);
-        // this.root.addEventListener('click', this.testReordering);
+       this.root.addEventListener('change', this.updateRating);
+
+       if (this.reloadButton) {
+           this.reloadButton.addEventListener('click', this.reloadPage);
+        }
+
+        // get page from url
+        const urlParts = window.location.pathname.split('/');
+        const page = urlParts[urlParts.length - 2];
+        if (!isNaN(page)) {
+            this.page = parseInt(page, 10) - 1;
+        }
+
+        // set current pageination item
+        const paginationItems = document.querySelectorAll(`${this.paginationsSelector} .elementor-icon-list-text`);
+        paginationItems.forEach(paginationItem => {
+            if (this.page === parseInt(paginationItem.textContent, 10) - 1) {
+                paginationItem.classList.add('current');
+            }
+        });
 
         // init masonry layout with Colcade
         this.colcade = new Colcade(this.root, {
@@ -229,18 +230,18 @@ class Feed {
         this.renderItems(firstItems);
     }
 
-    testReordering({ target }) {
-        if (target.id !== 'feed') {
-            return;
-        }
-        
-        // remove all children
+    async reloadPage({ target }) {       
+        // fetch items
+        const newItems = await this.fetchItems();
+    
+        // remove old items
         const oldItems = this.root.querySelectorAll('.feedItem');
         for (let i = 0; i < oldItems.length; i++) {
             oldItems[i].remove();
         }
-        const sortedItems = this.itemsElements.sort((a, b) => parseInt(b.dataset.rating, 10) - parseInt(a.dataset.rating, 10));
-        this.colcade.append(sortedItems);
+
+        // render new items
+        this.renderItems(newItems);
     }
 
     async fetchItems() {
@@ -248,7 +249,7 @@ class Feed {
         const results = await fetch(`${apiUrl}/social-wall?page=${this.page}`);
         const json = await results.json();
 
-        this.page += 1;
+        // const json = socialWallTestData[this.page];
         this.isFetching = false;
 
         return json;
@@ -258,38 +259,41 @@ class Feed {
         console.log('render items', data);
         // create items
         data.forEach((itemData, index) => {
-            const item = new Post(itemData);
-            this.items.push(item);
-            
-            const itemElement = item.createElement(index);
-            this.itemsElements.push(itemElement);
+            switch (itemData.post_type) {
+                case 'post': {
+                    const categories = itemData.categories.map(cat => cat.slug)
+                    if (categories.includes('featured')) {
+                        const featureElement = this.createFeatureElement(itemData);
+                        this.colcade.append(featureElement);
+                    } else if (categories.includes('frage')) {
+                        const questionElement = this.createQuestionElement(itemData);
+                        this.colcade.append(questionElement);
+                    }
+                    break;
+                }
+               
+                case 'flockler_post': {
+                    const item = new Post(itemData);
+                    this.items.push(item);
 
-            this.colcade.append(itemElement);
+                    const itemElement = item.createElement(index);
+                    this.itemsElements.push(itemElement);
+
+                    this.colcade.append(itemElement);
+                    break;
+                }
+                
+                default:
+                    break;
+            }
 
             // you
             if (index % 10 === 0) {
                 const youTile = this.youTile();
                 this.colcade.append(youTile);
                 this.youIndex += 1;
-                // this.itemsElements.push(youTile);
             }
-
-            // question
-            if (index % 15 === 0 && this.questionIndex < this.questions.length) {
-                const questionTile = this.questionTile();
-                this.colcade.append(questionTile);
-                // this.itemsElements.push(questionTile);
-                this.questionIndex += 1;
-            }
-            
-            // return itemElement;
         });
-        
-        // add items to feed
-        // Promise.all(items).then(items => {
-        //     this.colcade.append(items);
-        //     this.colcade.append(this.youTile());
-        // });
     }
 
     youTile() {
@@ -304,24 +308,22 @@ class Feed {
         return element;
     }
 
-    questionTile() {
-        const question = this.questions[this.questionIndex];
+    createQuestionElement(questionPostData) {
         const element = document.createElement('div');
         element.className = 'feedItem feedItem--question';
         element.innerHTML = `
-            <h3>${question.post_title}</h3>
+            <h3>${questionPostData.post_title}</h3>
         `;
         return element;
     }
 
-    async updateItems() {
-        if (!this.isFetching && this.page < 4) {
-            const triggerItem = this.itemsElements[this.items.length - 11]; // tenth last items
-            if (triggerItem && triggerItem.getBoundingClientRect().top <= window.innerHeight) {
-                const items = await this.fetchItems();
-                this.renderItems(items);
-            }
-        }
+    createFeatureElement(featurePostData) {
+        const element = document.createElement('div');
+        element.className = 'feedItem feedItem--feature';
+        element.innerHTML = `
+            <h3>${featurePostData.post_title}</h3>
+        `;
+        return element;
     }
 
     async updateRating({ target }) {
@@ -349,10 +351,13 @@ class Feed {
                 target.parentNode.previousElementSibling.style.display = 'none';
                 const feedItem = target.closest('.feedItem');
                 const itemIndex = this.itemsElements.indexOf(feedItem);
-                this.items[itemIndex].updateRating(newRatingString)
+                this.items[itemIndex].updateRating(newRatingString);
             }
         }
     }
 }
 
-new Feed('#feed');
+new Feed('#feed', {
+    reloadButton: '#feed-reload-button',
+    paginationsSelector: '.feed-pagination'
+});
