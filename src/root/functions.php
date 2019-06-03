fcf4d7106ca39ce34e789bef535acc712a7c056f
Daniel Högel <hoegel.daniel@gmail.com>
2019-06-03 03:53:42 +0200

    Accept line breaks in post excerpt

--- src/root/functions.php
+++ src/root/functions.php
@@ -10,6 +10,7 @@ require_once(__DIR__ . '/includes/elementor_hello_theme_setup.php');
 require_once(__DIR__ . '/includes/shortcodes.php');
 require_once(__DIR__ . '/includes/ajax_handler.php');
 require_once(__DIR__ . '/includes/feed_api/feed_api.php');
+require_once(__DIR__ . '/includes/custom_post_queries.php');
 
 
 /*** Load JS files ***/
@@ -29,3 +30,38 @@ function project_zukunft_scripts() {
         ]
     );
 }
+
+
+// append to themes/{your_theme}/functions.php
+
+define('EXCERPT_RARELY', '{[}]');
+define('EXCERPT_BR', nl2br(PHP_EOL));
+
+function wp_trim_excerpt_custom($text = '')
+{
+    add_filter('the_content', 'wp_trim_excerpt_custom_mark', 6);
+
+    // get through origin filter
+    $text = wp_trim_excerpt($text);
+
+    remove_filter('the_content', 'wp_trim_excerpt_custom_mark');
+
+    return wp_trim_excerpt_custom_restore($text);
+}
+
+function wp_trim_excerpt_custom_mark($text)
+{
+    $text = nl2br($text);
+    return str_replace(EXCERPT_BR, EXCERPT_RARELY, $text);
+}
+
+function wp_trim_excerpt_custom_restore($text)
+{
+    return str_replace(EXCERPT_RARELY, EXCERPT_BR, $text);
+}
+
+// remove default filter
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+
+// add custom filter
+add_filter('get_the_excerpt', 'wp_trim_excerpt_custom');
