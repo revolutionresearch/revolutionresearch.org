0360daed5de601cab4c73ca35ae41cf8d0e32220
Daniel Högel <hoegel.daniel@gmail.com>
2019-06-22 11:53:48 +0200

    Exclude specific users from manifesto revisions and only show same user once per day

--- src/root/includes/shortcodes.php
+++ src/root/includes/shortcodes.php
@@ -63,38 +63,48 @@ function wolfram_alpha_search_box_func( $attributes, $content ){
  */
 add_shortcode( 'berlin_manifesto_revisions', 'berlin_manifesto_revisions_func' );
 function berlin_manifesto_revisions_func( $attributes, $content ){
-    $POST_ID = 2296;
+    $POST_ID = 2296; // berlin-manifesto
     $revisions = wp_get_post_revisions($POST_ID);
 	
-	$result = '';
-	$index = 1;	
-    $revisions_count = count($revisions);
-    $max_revisions = 7;
-    $max_count = ($revisions_count < $max_revisions) ? $revisions_count : $max_revisions;
+    $max_count = 7;
+    
+    $excluded_authors_by_username = ['daniel.hoegel'];
+    $dates_by_user = [];
+	$results = [];
 
 	foreach($revisions as $post_id => $rev) {
+        $author_id = $rev->post_author;
+        $author =  get_userdata($author_id);
+
+        $username = $author->user_login;
         $date = get_the_date('d.m.Y', $post_id);
         
-        $author_id = $rev->post_author;
-        $single_value = true;
-		$nickname = get_user_meta($author_id, 'nickname', $single_value);
-		$first_name = get_user_meta($author_id, 'first_name', $single_value);
-        $last_name = get_user_meta($author_id, 'last_name', $single_value);
-    
-        // use first_name and last_name, only first_name or username
-        $name = $first_name ? (
-            $last_name ? "$first_name $last_name" : $first_name
-        ) : $nickname;
-		
-		$result = $result . "<span style='color: #ffff00;'><em>$name</em></span> am $date";
-		
-		if ($index < $max_count) {
-            $result .= ',<br />';
-            $index ++;
-		} else {
+        // excluded specific users like revisors or admins
+        if (in_array($username, $excluded_authors_by_username)) {
+            continue;
+        }
+        
+        // only show one entry for each user at the same day
+        if (isset($dates_by_user[$username]) && in_array($date, $dates_by_user[$username])) {
+            continue;
+        }
+        
+        // create html string
+        $name = $author->display_name;
+        array_push($results, "<span style='color: #ffff00;'><em>$name</em></span> am $date");
+
+        // store dates by username to exclude multiple edits on the same day
+        if (isset($dates_by_user[$username])) {
+            array_push($dates_by_user[$username], $date);
+        } else {
+            $dates_by_user[$username] = [$date];
+        }
+        
+        // break if max count is reached
+		if (count($results) >= $max_count) {
             break;
         }
 	}
 	
-	return $result;
+	return join(',<br />', $results);
 }
