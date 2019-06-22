6b337057c47347c4c7a51f37a370e33ac12fa3eb
Daniel Högel <hoegel.daniel@gmail.com>
2019-06-23 01:40:06 +0200

    Style event filters

--- src/js/events_filter.js
+++ src/js/events_filter.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('events-filter');
-
-    if (form) {
-        // add default values from url params
+(function() {
+    /**
+     * Input default values
+     */
+    function setDefaultInputValues(form) {
         const urlParams = new URLSearchParams(window.location.search);
         for(var pair of urlParams.entries()) {
             const name = pair[0];
@@ -12,19 +12,42 @@ document.addEventListener('DOMContentLoaded', () => {
                 const input = form.querySelector(`[name="${name}"]`);
                 if (input) {
                     input.value = value;
+                    input.classList.add('has-value');
                 }
             }
         }
-
-
-
-        // submit after change
-        form.addEventListener('change', () => {
-            document.querySelectorAll('.event-list').forEach(
-                eventsList => eventsList.classList.add('disabled')
-            );
-            form.classList.add('disabled');
-            form.submit();
-        });
     }
-});
+
+
+    /**
+     * Input change handler
+     */
+    function inputChangeHandler(e) {
+        const input = e.target;
+        const form = e.currentTarget;
+
+        // add or remove has-value class 
+        input.classList.toggle('has-value', input.value !== '');
+        
+        // disable form and event lists
+        form.classList.add('disabled');
+        document.querySelectorAll('.event-list').forEach(
+            eventsList => eventsList.classList.add('disabled')
+        );
+        
+        // submit form and reload page
+        form.submit();
+    }
+
+
+    /**
+     * MAIN
+     */
+    document.addEventListener('DOMContentLoaded', () => {
+        const form = document.getElementById('events-filter');
+        if (form) {
+            setDefaultInputValues(form)
+            form.addEventListener('change', inputChangeHandler);
+        }
+    });    
+})()
