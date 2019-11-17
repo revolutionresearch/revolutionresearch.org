ba90dcb344efd646f31ce1aa36aff12730b3c61c
Daniel Högel <hoegel.daniel@gmail.com>
2019-11-17 12:38:21 +0100

    Fix revision minutes (m = month, i = minutes)

--- src/root/includes/shortcodes/wiki_articles_revisions_shortcode.php
+++ src/root/includes/shortcodes/wiki_articles_revisions_shortcode.php
@@ -49,7 +49,7 @@ function wiki_articles_revisions_func( $attributes, $content ) {
         // get date and time
         $timestamp = strtotime($rev->post_date);
         $date = date('d.m.Y', $timestamp);
-        $time = date('H:m', $timestamp);
+        $time = date('H:i', $timestamp);
         
         // only show one entry for each user at the same day
         if (isset($dates_by_user[$username]) && in_array($date, $dates_by_user[$username])) {
